dbca09f1c79ddbc17d8cd4c6881a5830d98b760f
Shubham Samarth <shubhams@heaptrace.net>
2024-06-13 19:35:18 +0530

    config: tailwind

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./tailwind.css";
+
+const App = () => {
+  return <div className="underline">App</div>;
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}", // Adjust according to your project's file structure
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
