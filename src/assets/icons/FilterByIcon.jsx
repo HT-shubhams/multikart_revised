e933786b263c34e3cf1f677f184c3c35e04a3a63
Shubham Samarth <shubhams@heaptrace.net>
2024-06-14 18:17:26 +0530

    fix: icons revised

--- src/assets/icons/FilterByIcon.jsx
+++ src/assets/icons/FilterByIcon.jsx
@@ -4,7 +4,7 @@ export const FilterByIcon = ({ className }) => {
   return (
     <svg
       width="20"
-      height="20"
+      height="18"
       viewBox="0 0 20 20"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,5 +1,11 @@
 import React from "react";
-import { GridViewIcon, ListViewIcon } from "../assets/icons";
+import {
+  FilterByIcon,
+  GridViewIcon,
+  ListViewIcon,
+  SearchIcon,
+  SortByIcon,
+} from "../assets/icons";
 
 const UserList = () => {
   return (
@@ -19,7 +25,24 @@ const UserList = () => {
         </div>
       </div>
       <div className="flex mt-4 md:mt-8 rounded-lg shadow-md mx-2 md:mx-9">
-        <div className="">UserList container</div>
+        <div className="flex justify-between w-full ">
+          <div className="flex space-x-3">
+            <button className="flex p-2 border border-[#777a81] rounded-md items-center">
+              <SortByIcon className={"md:mr-2"} />
+              <span className="hidden md:inline">Sort By</span>
+            </button>
+            <button className="flex p-2 border border-[#777a81] rounded-md">
+              <FilterByIcon className={"md:mr-1 my-auto"} />
+              <span className="hidden md:inline">Filter By</span>
+            </button>
+          </div>
+
+          <div className="flex">
+            <div className="border p-2 md:p-3 border-[#777a81] rounded-md items-center">
+              <SearchIcon className={"my-auto"} />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
