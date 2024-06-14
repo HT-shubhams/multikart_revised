988fcf4fefa88ff24ca55c0555b8ce5ad34a08f1
Shubham Samarth <shubhams@heaptrace.net>
2024-06-14 16:01:46 +0530

    fix: basic UserList Container

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -13,10 +13,13 @@ const UserList = () => {
           <button className="bg-white p-2 rounded-r-md border-2 border-gray-300 ml-0">
             <GridViewIcon className="w-5 h-5 text-[#641CC0]" />
           </button>
-          <button className="bg-[#641CC0] text-white p-2 rounded-md w-20 h-10 md:w-40 md:h-12 md:w-20 md:h-10 ml-3 md:ml-5 mr-5 md:mr-9">
+          <button className="bg-[#641CC0] text-white p-2 rounded-md w-20 h-10 md:w-40 md:h-12 ml-3 md:ml-5 mr-5 md:mr-9">
             {window.innerWidth < 768 ? "+ Add" : "+ Add User"}
           </button>
         </div>
+      </div>
+      <div className="flex mt-4 md:mt-8 rounded-lg shadow-md mx-2 md:mx-9">
+        <div className="">UserList container</div>
       </div>
     </div>
   );
