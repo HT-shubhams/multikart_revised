c1528a564cdde26bd6056a4c35e7335f38e3e876
Shubham Samarth <shubhams@heaptrace.net>
2024-06-15 11:59:31 +0530

    fix: search bar responsive

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import classNames from "classnames";
 import {
   FilterByIcon,
   GridViewIcon,
@@ -8,11 +9,14 @@ import {
 } from "../assets/icons";
 
 const UserList = () => {
+  const [isInputFocused, setIsInputFocused] = useState(false);
+  const [isSearchExpanded, setIsSearchExpanded] = useState(false);
+
   return (
     <div className="bg-[#F9F9F9]">
       <div className="flex justify-between mt-8">
         <div className="text-2xl ml-4 md:ml-8">Users</div>
-        <div className="flex ">
+        <div className="flex">
           <button className="bg-[#641CC0] text-white p-2 rounded-l-md">
             <ListViewIcon className="w-5 h-5" />
           </button>
@@ -20,32 +24,50 @@ const UserList = () => {
             <GridViewIcon className="w-5 h-5 text-[#641CC0]" />
           </button>
           <button className="bg-[#641CC0] text-white p-2 rounded-md w-20 h-10 md:w-40 md:h-12 ml-3 md:ml-5 mr-5 md:mr-9">
-            {/* {window.innerWidth < 768 ? "+ Add" : "+ Add User"} */}
             <span className="md:hidden">+ Add</span>
             <span className="hidden md:block">+ Add User</span>
           </button>
         </div>
       </div>
       <div className="flex mt-4 md:mt-8 rounded-lg shadow-md mx-2 md:mx-9">
-        <div className="flex justify-between w-full ">
+        <div className="flex justify-between w-full">
           <div className="flex space-x-3">
             <button className="flex p-2 border border-[#777a81] rounded-md items-center">
-              <SortByIcon className={"md:mr-2"} />
+              <SortByIcon className="md:mr-2" />
               <span className="hidden md:inline">Sort By</span>
             </button>
             <button className="flex p-2 border border-[#777a81] rounded-md">
-              <FilterByIcon className={"md:mr-1 my-auto"} />
+              <FilterByIcon className="md:mr-1 my-auto" />
               <span className="hidden md:inline">Filter By</span>
             </button>
           </div>
 
-          <div className="flex">
-            <div className="flex border p-2 md:p-3 border-[#777a81] rounded-md items-center">
-              <SearchIcon className={"my-auto"} />{" "}
+          <div className="flex items-center">
+            <div
+              className={classNames(
+                "flex items-center gap-1 bg-white py-2 pl-2 pr-[4.5px] md:p-2 rounded-lg border border-[#777a81]",
+                { "!border-[#2e4272]": isInputFocused }
+              )}
+            >
+              <button
+                type="button"
+                className="h-6 aspect-square"
+                onClick={() => setIsSearchExpanded(!isSearchExpanded)}
+              >
+                <SearchIcon className="w-5 h-5" />
+              </button>
               <input
                 type="text"
-                placeholder="Search here"
-                className="outline-none hidden md:ml-2 md:block md:w-52"
+                placeholder="Search"
+                className={classNames(
+                  "bg-transparent outline-none text-black",
+                  { "hidden md:block": !isSearchExpanded }
+                )}
+                onBlur={() => {
+                  setIsInputFocused(false);
+                  setIsSearchExpanded(false);
+                }}
+                onFocus={() => setIsInputFocused(true)}
               />
             </div>
           </div>
