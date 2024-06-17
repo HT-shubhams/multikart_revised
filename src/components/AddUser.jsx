2956d152e9172bddce46156f0f38a0083753ac7c
Shubham Samarth <shubhams@heaptrace.net>
2024-06-17 15:29:21 +0530

    addUser headings

--- src/components/AddUser.jsx
+++ src/components/AddUser.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { HomeIcon } from "../assets/icons/HomeIcon";
 
 const AddUser = () => {
-  return <div>AddUser</div>;
+  return (
+    <div>
+      <div className="mt-8 md:mt-11 md:flex md:justify-between md:items-center md:mr-12 text-[#63666b]">
+        <div className="text-2xl ml-4 md:ml-8 text-black">Users</div>
+        <div className="flex ml-4 mt-3 md:mt-0 md:ml-8 items-center text-sm md:text-lg">
+          <div className="pr-2 md:pr-4">
+            <HomeIcon />
+          </div>
+          / <span className="px-2 md:px-4 text-black">Users</span> /{" "}
+          <span className="px-2 md:px-4">Add User</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AddUser;
