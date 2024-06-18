15a9e2af9b4a4aabec3428947b62f2a9473820b8
Shubham Samarth <shubhams@heaptrace.net>
2024-06-18 15:28:29 +0530

    fix: routes on clicking Users and Add User and Edit User

--- src/components/AddUser.jsx
+++ src/components/AddUser.jsx
@@ -60,11 +60,17 @@ const AddUser = () => {
       <div className="mt-8 md:mt-11 md:flex md:justify-between md:items-center md:mr-12 text-[#63666b]">
         <div className="text-2xl ml-4 md:ml-8 text-black">Users</div>
         <div className="flex ml-4 mt-3 md:mt-0 md:ml-8 items-center text-sm md:text-lg">
-          <div className="pr-2 md:pr-4">
+          <div className="pr-2 md:pr-4" onClick={() => navigate("/")}>
             <HomeIcon />
           </div>
-          / <span className="px-2 md:px-4 text-black">Users</span> /{" "}
-          <span className="px-2 md:px-4">Add User</span>
+          /{" "}
+          <span
+            className="px-2 md:px-4 text-black"
+            onClick={() => navigate("/")}
+          >
+            Users
+          </span>{" "}
+          / <span className="px-2 md:px-4">Add User</span>
         </div>
       </div>
 
--- src/components/EditUser.jsx
+++ src/components/EditUser.jsx
@@ -58,11 +58,17 @@ const EditUser = () => {
       <div className="mt-8 md:mt-11 md:flex md:justify-between md:items-center md:mr-12 text-[#63666b]">
         <div className="text-2xl ml-4 md:ml-8 text-black">Users</div>
         <div className="flex ml-4 mt-3 md:mt-0 md:ml-8 items-center text-sm md:text-lg">
-          <div className="pr-2 md:pr-4">
+          <div className="pr-2 md:pr-4" onClick={() => navigate("/")}>
             <HomeIcon />
           </div>
-          / <span className="px-2 md:px-4 text-black">Users</span> /{" "}
-          <span className="px-2 md:px-4">Edit User</span>
+          /{" "}
+          <span
+            className="px-2 md:px-4 text-black"
+            onClick={() => navigate("/")}
+          >
+            Users
+          </span>{" "}
+          / <span className="px-2 md:px-4">Edit User</span>
         </div>
       </div>
 
