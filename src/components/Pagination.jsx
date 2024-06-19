cf3780875233e624e0a21f9fe47814a818231a2c
Shubham Samarth <shubhams@heaptrace.net>
2024-06-19 16:37:31 +0530

    fix: pagination styles

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -20,41 +20,44 @@ const Pagination = ({
   };
 
   return (
-    <div className="m-2 flex justify-between text-[#63666b]">
+    <div className="m-2 flex justify-between text-[#63666b] md:justify-end">
       <div className="text-xs font-normal md:text-base">
         <span>Items per page:</span>
         <input
           type="number"
           value={itemsPerPage}
           onChange={(e) => onItemsPerPageChange(Number(e.target.value))}
-          className="w-7 border-b-2 border-[#e5e5e5] md:w-14"
+          className="w-7 border-b-2 border-[#e5e5e5] md:w-10"
         />
       </div>
-      <div className="text-xs font-normal md:text-base">
-        {`Displaying ${itemsPerPage * (currentPage - 1) + 1}-${Math.min(
-          itemsPerPage * currentPage,
-          totalRecords,
-        )} of ${totalRecords} records`}
-      </div>
+
       <div className="flex">
-        <button
-          onClick={handlePrevPage}
-          disabled={currentPage === 1}
-          className={classNames(
-            "p-2 disabled:cursor-not-allowed disabled:opacity-50",
-          )}
-        >
-          {LeftArrowIcon ? <LeftArrowIcon /> : "<"}
-        </button>
-        <button
-          onClick={handleNextPage}
-          disabled={currentPage === totalPages}
-          className={classNames(
-            "p-2 disabled:cursor-not-allowed disabled:opacity-50",
-          )}
-        >
-          {RightArrowIcon ? <RightArrowIcon /> : ">"}
-        </button>
+        <div className="ml-2 text-xs font-normal md:text-base">
+          {`${itemsPerPage * (currentPage - 1) + 1}-${Math.min(
+            itemsPerPage * currentPage,
+            totalRecords,
+          )} of ${totalRecords}`}
+        </div>
+        <div className="flex">
+          <button
+            onClick={handlePrevPage}
+            disabled={currentPage === 1}
+            className={classNames(
+              "p-1 disabled:cursor-not-allowed disabled:opacity-50 md:p-2",
+            )}
+          >
+            {LeftArrowIcon ? <LeftArrowIcon /> : "<"}
+          </button>
+          <button
+            onClick={handleNextPage}
+            disabled={currentPage === totalPages}
+            className={classNames(
+              "p-1 disabled:cursor-not-allowed disabled:opacity-50 md:p-2",
+            )}
+          >
+            {RightArrowIcon ? <RightArrowIcon /> : ">"}
+          </button>
+        </div>
       </div>
     </div>
   );
