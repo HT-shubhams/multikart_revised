7b099d06ed9de3af522d40be1654578bbaea7057
Shubham Samarth <shubhams@heaptrace.net>
2024-06-26 12:00:54 +0530

    Design: Mainpage Header

--- src/components/UserSignup.jsx
+++ src/components/UserSignup.jsx
@@ -1,5 +1,13 @@
 import React from "react";
+import { MainpageHeader } from "./MainpageHeader";
 
 export const UserSignup = () => {
-  return <div>UserSignup</div>;
+  return (
+    <div>
+      <div>
+        <MainpageHeader />
+      </div>
+      UserSignup
+    </div>
+  );
 };
